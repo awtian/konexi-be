718321a7c368acf09cfd7dca87811befe8a1d808
Awtian <me@awtian.com>
2023-05-10 23:46:17 +0700

    fixing status codes

--- resources/user/user.controller.js
+++ resources/user/user.controller.js
@@ -1,7 +1,104 @@
-const {signup, signin, update} = require('../../utils/auth');
+const { newToken, verifyToken } = require("../../utils/auth");
+const { User } = require("./user.model");
 
 module.exports = {
-  signup,
-  signin,
-  update
-}
+  signup: async (req, res) => {
+    try {
+      // Payload check
+      if (!req.body.username || !req.body.password || !req.body.fullName) {
+        return res
+          .status(422)
+          .send({ message: "username, fullName, and password are required!" });
+      }
+      // Creating user
+      const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        fullName: req.body.fullName,
+      });
+      // generate token
+      const token = newToken(user);
+      // send token to client
+      return res.status(201).send({ token });
+    } catch (err) {
+      console.error(err);
+      if (err.code === 11000) {
+        return res
+          .status(409)
+          .send({ message: "The username is in use by other user" });
+      } else {
+        return res.status(400).end();
+      }
+    }
+  },
+  signin: async (req, res) => {
+    // payload check
+    if (!req.body.username || !req.body.password) {
+      return res
+        .status(422)
+        .send({ message: "Username and password is required!" });
+    }
+
+    // username check
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res
+        .status(401)
+        .send({ message: "Not auth: Username does not exist" });
+    }
+
+    try {
+      // password check
+      const match = await user.checkPassword(req.body.password);
+      if (!match) {
+        return res.status(401).send({ message: "Not auth: Invalid password" });
+      } else {
+        // user exist, generate token
+        const token = newToken(user);
+        res
+          .status(200)
+          .send({ token, fullName: user.fullName, role: user.role });
+      }
+    } catch (e) {
+      console.error(e);
+      return res.status(400).send({ message: e });
+    }
+  },
+  update: async (req, res) => {
+    let token = req.headers.authorization?.split("Bearer ")[1];
+
+    if (!token) {
+      return res
+        .status(401)
+        .send({ message: "Not auth: Token is not exist or on a wrong format" });
+    }
+
+    try {
+      const payload = await verifyToken(token);
+      if (payload) {
+        let data = await User.findOne({ _id: payload.id });
+        if (req.body.password) {
+          data.password = req.body.password;
+        }
+        data.username = req.body.username ? req.body.username : data.username;
+        data.fullName = req.body.fullName ? req.body.fullName : data.fullName;
+        await data.save();
+        res.status(201).send({
+          message: "User updated",
+          data: { username: data.username, fullName: data.fullName },
+        });
+      } else {
+        res.status(401).send({ message: "Not auth: Username does not exist" });
+      }
+    } catch (e) {
+      if (e.code === 11000) {
+        return res
+          .status(409)
+          .send({ message: "Your new username is in use by other user" });
+      } else {
+        console.error(e);
+        return res.status(400).end();
+      }
+    }
+  },
+};
--- utils/auth.js
+++ utils/auth.js
@@ -15,93 +15,6 @@ const verifyToken = (token) =>
     });
   });
 
-const signup = async (req, res) => {
-  try {
-    if (!req.body.username || !req.body.password || !req.body.fullName) {
-      return res
-        .status(401)
-        .send({ message: "username, fullName, and password is required!" });
-    }
-    const user = await User.create({
-      username: req.body.username,
-      password: req.body.password,
-      fullName: req.body.fullName,
-    });
-    const token = newToken(user);
-    return res.status(201).send({ token });
-  } catch (e) {
-    console.error(e);
-    // res.send(e);
-    return res.status(400).end();
-  }
-};
-
-const signin = async (req, res) => {
-  if (!req.body.username || !req.body.password) {
-    return res
-      .status(401)
-      .send({ message: "Username and password is required!" });
-  }
-
-  const user = await User.findOne({ username: req.body.username });
-  if (!user) {
-    return res
-      .status(400)
-      .send({ message: "Not auth: Username does not exist" });
-  }
-
-  try {
-    const match = await user.checkPassword(req.body.password);
-    console.log(match);
-    if (!match) {
-      return res.status(401).send({ message: "Not auth: Invalid password" });
-    } else {
-      const token = newToken(user);
-      res.status(200).send({ token, fullName: user.fullName, role: user.role });
-    }
-  } catch (e) {
-    console.error(e);
-    return res.status(400).send({ message: e });
-  }
-};
-
-const update = async (req, res) => {
-  const password = req.body.password;
-
-  let token = req.headers.authorization?.split("Bearer ")[1];
-
-  if (!token) {
-    return res
-      .status(401)
-      .send({ message: "Not auth: Token is not exist or on a wrong format" });
-  }
-
-  try {
-    const payload = await verifyToken(token);
-    if (payload) {
-      let data = await User.findOne({ _id: payload.id });
-      data.password = password;
-      data.username = req.body.username ? req.body.username : data.username;
-      data.fullName = req.body.fullName ? req.body.fullName : data.fullName;
-      await data.save();
-      res.status(201).send({
-        message: "User updated",
-        data: { username: data.username, fullName: data.fullName },
-      });
-    } else {
-      res.status(400).send({ message: "Not auth: Username does not exist" });
-    }
-  } catch (e) {
-    console.error(e);
-    if (e.code === 11000) {
-      return res
-        .status(400)
-        .send({ message: "Your new username has been used by other user" });
-    }
-    return res.status(400).send({ message: e });
-  }
-};
-
 const protect = async (req, res, next) => {
   if (!req.headers.authorization) {
     return res.status(401).send({ message: "Not auth: Token does not exist" });
@@ -165,9 +78,8 @@ const admin = async (req, res, next) => {
 };
 
 module.exports = {
-  signup,
-  signin,
+  newToken,
+  verifyToken,
   protect,
-  update,
   admin,
 };
